fe3d33f3c30b72cac2e6b90d8bebd207db10d1e7
Jimmy Koop <jimmy.koop@adesso.de>
2019-10-29 12:24:09 +0100

    layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { BasicLayoutModule } from './basic-layout/basic-layout.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    BasicLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
